be3d9a9418bb3201a6ac7781123482e036f3b06a
Bulat Cherkashin <busyokbedlam@gmail.com>
2020-02-18 11:46:26 +0200

    Rewrite Builder Pattern

--- builder/index.js
+++ builder/index.js
@@ -27,4 +27,30 @@ class CarDirector {
   }
 }
 
-export default CarDirector;
+class CarBuilder {
+  constructor() {
+    this.car = Object.create(null);
+  }
+  setEngine(engine) {
+    this.car.engine = engine;
+    return this;
+  }
+  setParktronic(parktronic) {
+    this.car.parktronic = parktronic;
+    return this;
+  }
+  setSignalling(signalling) {
+    this.car.signalling = signalling;
+    return this;
+  }
+
+  build() {
+    return this.car;
+  }
+}
+
+const director = new CarDirector();
+const builder1 = new CarBuilder();
+director.setOrder({ engine: 'engine1', parktronic: 'parktronic1', signalling: 'signalling1' });
+let result = director.constructCar(builder1, 0);
+console.log('RESULT', result);
